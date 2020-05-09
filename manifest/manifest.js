a650096009ffd816471e7ac7bde236bc2d70f4a7
Nidza <zlatnaspirala@gmail.com>
2020-05-09 16:57:37 +0200

    initial good standing point

---
+++ manifest/manifest.js
@@ -0,0 +1,38 @@
+/**
+ * Filename : manifest.js
+ * created by Nikola Lukic zlatnaspirala@gmail.com
+ * https://maximumroulette.com 2016-2020
+ */
+
+var APPLICATION = {
+  NAME: "WOLF-360-PLAYER",
+  TYPE: "client",
+  VERSION: "0.1",
+  STATUS: "develop",
+  DEVELOPERS: ["Nikola Lukic Zlatnaspirala@gmail.com"],
+  PROGRAM: {}
+};
+
+APPLICATION.PROGRAM = PROGRAM;
+
+var PROGRAM = {
+  NAME: "Three.js used ,based on visula-js",
+  GRID: false,
+  AUTO_UPDATE: [],
+  ANTIALIAS: true,
+  SKY: false,
+  LIGHTS: true,
+  DDSLoader: true,
+  OBJLoader: true,
+  MTLLoader: true,
+  MD2Loader: true,
+  ColladaLoader: false,
+  MD2Character: true,
+  FBXLoader: false,
+  GROUND: true,
+  RAYCAST: true,
+  PARTICLE_ENGINE: false,
+  MATERIALS: true,
+  STREAM_TEXTURE: true,
+  PANORAMA_TEXTURES: true
+};
