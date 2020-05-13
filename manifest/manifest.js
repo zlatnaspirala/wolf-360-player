d7f1b8434cacd7460fc0294668ce933801c05500
Nidza <zlatnaspirala@gmail.com>
2020-05-13 17:54:53 +0200

    release test beta

--- manifest/manifest.js
+++ manifest/manifest.js
@@ -9,13 +9,14 @@ var APPLICATION = {
   TYPE: "client app",
   VERSION: "0.1",
   STATUS: "develop",
-  DEVELOPERS: ["Nikola Lukic zlatnaspirala@gmail.com"],
-  PROGRAM: {
-    NAME: "Three.js used ,based on visula-js",
-    GRID: false,
-    AUTO_UPDATE: [],
-    ANTIALIAS: true,
-    SKY: false,
-    LIGHTS: true
-  }
+  DEVELOPERS: ["Nikola Lukic zlatnaspirala@gmail.com"]
+};
+
+var PROGRAM = {
+  NAME: "Three.js used ,based on visula-js",
+  GRID: false,
+  AUTO_UPDATE: [],
+  ANTIALIAS: true,
+  SKY: false,
+  LIGHTS: true
 };
